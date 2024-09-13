b04c8c6af5f6220ffa5a5a94f1f2d796b3b934cf
RonNahmany <ronnahmany@gmail.com>
2024-09-13 21:43:10 +0300

    terraform changes

---
+++ module/variables.tf
@@ -0,0 +1,22 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "il-central-1"
+}
+variable "vpc_name" {
+  description = "VPC name of service"
+  type        = string
+  default     = "il-central-1"
+}
+
+variable "instance_types" {
+  description = "free tier"
+  type        = map(string)
+  default     = {
+    "freetier"  = "t2.micro"
+    "db"   = "t2.medium"
+    "cache" = "t3.micro"
+  }
+}
+
+
